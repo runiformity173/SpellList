df1f3ae859b3ac5b8e68e19e0eeba658d8ff265c
Eli Harrison <eli.z.harrison@gmail.com>
2026-09-15 12:49:00 -0400

    Automatically populate spell source filters

--- generateDataFromJSON.py
+++ generateDataFromJSON.py
@@ -8,6 +8,8 @@ for key in index:
     with open("data/"+index[key],"r") as fl:
         spells.extend(json.load(fl)["spell"])
 
+foundSources = {}
+
 with open("data/spell-sources.json","r") as fl:
     spellClasses = json.load(fl)
 for spell in spells:
@@ -23,6 +25,9 @@ for spell in spells:
     except:
         pass
     spell["classes"] = list(srcs)
+    src = spell["source"]
+    if src in foundSources: foundSources[src] += 1
+    else: foundSources[src] = 1
     
 
 final += "const spells = " + json.dumps(spells) + ";\n"
@@ -31,10 +36,13 @@ sourceDict = {}
 with open("data/books.json","r") as fl:
     books = json.load(fl)["book"]
 for book in books:
+    if book["source"] not in foundSources: continue
     sourceDict[book["source"]] = book["name"]
 sourceDict["IDRotF"] = "Icewind Dale: Rime of the Frostmaiden"
 sourceDict["LLK"] = "Lost Laboratory of Kwalish"
 sourceDict["AitFR-AVT"] = "Adventures in the Forgotten Realms: A Verdant Tomb"
+
+sourceDict = dict(sorted(sourceDict.items(),key=lambda x:(-foundSources[x[0]],x[0])))
 
 final += "const sourceDict = " + json.dumps(sourceDict) + ";\n"
 
