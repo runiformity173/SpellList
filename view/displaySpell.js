30d54e24de0c59fe1328ff0ffe1cec8b94e96bdf
Eli Harrison <eli.z.harrison@gmail.com>
2026-09-16 08:57:09 -0400

    Revert "Prefix matching instead of whole string"
    This reverts commit a43d02309844509543c50b5269dd29f01e21d5b4.

--- view/displaySpell.js
+++ view/displaySpell.js
@@ -28,7 +28,7 @@ function getSpellByName(name,list=spells) {
         let bestDist = Infinity;
         for (const spell of list) {
             if (spell.reprintedAs) continue;
-            let dist = stringDistance(spellName, spell.name.toLowerCase().slice(0,spellName.length));
+            let dist = stringDistance(spellName, spell.name.toLowerCase())
             if (dist < bestDist) {
                 bestDist = dist;
                 best = spell;
